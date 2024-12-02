81d9af7183d55ca800c4655e490cb67e9c23f80c
trunghd1102 <minehd981@gmail.com>
2024-12-02 15:18:37 +0700

    commited

--- js/main.js
+++ js/main.js
@@ -7,13 +7,11 @@ btnLogin.addEventListener("click", (e) => {
   if (inputUsername.value === "" || inputPassword.value === "") {
     alert("Vui lòng không để trống");
   } else {
-    // Kiểm tra tài khoản và mật khẩu từ localStorage
     const user = JSON.parse(localStorage.getItem(inputUsername.value));
     if (user && user.username === inputUsername.value && user.password === inputPassword.value) {
       alert("Đăng Nhập Thành Công");
-      // Lưu thông tin đăng nhập vào sessionStorage
       sessionStorage.setItem('loggedInUser', inputUsername.value);
-      window.location.href = "index.html";  // Chuyển hướng về trang chủ
+      window.location.href = "index.html";
     } else {
       alert("Đăng Nhập Thất Bại hoặc tài khoản không tồn tại");
     }
